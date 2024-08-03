34f78526b791f0e9fcdc1e8ab2bf5e8ef37173b5
Haider-HH <Haider-HH>
2024-08-03 20:06:45 +0300

    orderStatusAdmin

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,8 +1,8 @@
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList, StyleSheet, Touchable, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import { router, Stack, useLocalSearchParams } from 'expo-router';
 import orders from '@/assets/data/orders';
-import { Order } from '@/src/types';
+import { Order, OrderStatus } from '@/src/types';
 import OrderItemListItem from '../../../components/OrderItemListItem';
 import OrderListItem from '../../../components/OrderListItem';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -11,6 +11,8 @@ import Colors from '@/src/constants/Colors';
 import Button from '@/src/components/Button';
 
 dayjs.extend(relativeTime);
+
+const status: OrderStatus[] = ['New', 'Cooking', 'Delivering', 'Delivered']
 
 const OrderDetails = () => {
     const { id } = useLocalSearchParams();
@@ -28,6 +30,8 @@ const OrderDetails = () => {
         );
     }
 
+    const [selectedStatus, setSelectedStatus] = useState<OrderStatus>(order.status);
+    // later, we will change it from the database
     return (
         <View style={styles.container}>
             <Stack.Screen options={{ title: `Order #${order.id}` }} />
@@ -36,12 +40,28 @@ const OrderDetails = () => {
                 data={order.order_items}
                 renderItem={({ item }) => <OrderItemListItem orderItem={item} />}
                 ListFooterComponent={
-                    <Text style={styles.totalPrice}>
-                        Total Price: {''}
-                        <Text style={{color: Colors.light.tint}}>
-                            ${order.total}
+                    <View>
+                        <Text style={styles.totalPrice}>
+                            Total Price: {''}
+                            <Text style={{color: Colors.light.tint}}>
+                                ${order.total}
+                            </Text>
                         </Text>
-                    </Text>
+                        <Text style={{fontWeight: 'bold', fontSize: 16}}>
+                            Status
+                        </Text>
+                        <View style={{flexDirection: 'row', marginTop: 10}}>
+                            {status.map((stat) => {
+                                return (
+                                    <TouchableOpacity key={stat} activeOpacity={0.7} onPress={() => setSelectedStatus(stat)}>
+                                        <View style={[styles.statusStyling, selectedStatus === stat && {backgroundColor: Colors.light.tint}]}>
+                                            <Text style={[selectedStatus === stat? {color: 'white'} : {color: Colors.light.tint}]}>{stat}</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                )
+                            })}
+                        </View>
+                    </View>
                 }
                 ListHeaderComponent={() => <OrderListItem order={order} />}
                 contentContainerStyle={styles.listContent}
@@ -62,7 +82,9 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20,
         marginTop: 20,
+        marginBottom: 20
     },
+    statusStyling: {borderRadius: 5, borderWidth: 1, padding: 10, marginRight: 10, borderColor: Colors.light.tint}
 });
 
 export default OrderDetails;
